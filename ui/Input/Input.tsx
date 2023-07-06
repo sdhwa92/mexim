0f457b28a6c112eacec39b7ae2a5f8dfe074af3b
Daehwa Seo <sdhwa92@gmail.com>
2023-07-06 22:13:35 +1000

    add style variant for error state

--- ui/Input/Input.tsx
+++ ui/Input/Input.tsx
@@ -2,13 +2,28 @@ import React from "react";
 import { FC } from "react";
 import { classNames } from "@/lib/utils";
 
-interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {}
+interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
+  error?: string;
+}
 
-const Input: FC<InputProps> = ({ className, ...props }) => {
+const Input: FC<InputProps> = ({ className, error, ...props }) => {
   return (
     <input
       className={classNames(
-        "block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6",
+        "block w-full rounded-md border-0 py-1.5 shadow-sm ring-1 ring-inset focus:ring-2 focus:ring-inset sm:text-sm sm:leading-6",
+        ...(!error
+          ? [
+              "text-gray-900",
+              "ring-gray-300",
+              "placeholder:text-gray-400",
+              "focus:ring-indigo-600",
+            ]
+          : [
+              "text-red-900",
+              "ring-red-300",
+              "placeholder:text-red-300",
+              "focus:ring-red-500",
+            ]),
         className
       )}
       {...props}
--- ui/Textarea/Textarea.tsx
+++ ui/Textarea/Textarea.tsx
@@ -3,13 +3,28 @@ import { FC } from "react";
 import { classNames } from "@/lib/utils";
 
 interface TextareaProps
-  extends React.TextareaHTMLAttributes<HTMLTextAreaElement> {}
+  extends React.TextareaHTMLAttributes<HTMLTextAreaElement> {
+  error?: string;
+}
 
-const Textarea: FC<TextareaProps> = ({ className, ...props }) => {
+const Textarea: FC<TextareaProps> = ({ className, error, ...props }) => {
   return (
     <textarea
       className={classNames(
-        "block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6",
+        "block w-full rounded-md border-0 py-1.5 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6",
+        ...(!error
+          ? [
+              "text-gray-900",
+              "ring-gray-300",
+              "placeholder:text-gray-400",
+              "focus:ring-indigo-600",
+            ]
+          : [
+              "text-red-900",
+              "ring-red-300",
+              "placeholder:text-red-300",
+              "focus:ring-red-500",
+            ]),
         className
       )}
       {...props}
