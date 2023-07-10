8e38b4fc3a2594e0d3bae6a755604cbb5bb9ce56
Daehwa Seo <sdhwa92@gmail.com>
2023-07-10 19:12:53 +1000

    fix the console error caused by textarea component

--- ui/Textarea/Textarea.tsx
+++ ui/Textarea/Textarea.tsx
@@ -1,5 +1,4 @@
-import React from "react";
-import { FC } from "react";
+import React, { FC } from "react";
 import { classNames } from "@/lib/utils";
 
 interface TextareaProps
@@ -7,28 +6,32 @@ interface TextareaProps
   error?: string;
 }
 
-const Textarea: FC<TextareaProps> = ({ className, error, ...props }) => {
-  return (
-    <textarea
-      className={classNames(
-        "block w-full rounded-md border-0 py-1.5 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6",
-        ...(!error
-          ? [
-              "text-gray-900",
-              "ring-gray-300",
-              "placeholder:text-gray-400",
-              "focus:ring-indigo-600",
-            ]
-          : [
-              "text-red-900",
-              "ring-red-300",
-              "placeholder:text-red-300",
-              "focus:ring-red-500",
-            ]),
-        className
-      )}
-      {...props}
-    />
+const Textarea: FC<TextareaProps> = ({ className, error, value, ...props }) => {
+  const textareaProps = {
+    ...props,
+    className: classNames(
+      "block w-full rounded-md border-0 py-1.5 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6",
+      !error
+        ? [
+            "text-gray-900",
+            "ring-gray-300",
+            "placeholder:text-gray-400",
+            "focus:ring-indigo-600",
+          ]
+        : [
+            "text-red-900",
+            "ring-red-300",
+            "placeholder:text-red-300",
+            "focus:ring-red-500",
+          ],
+      className
+    ),
+  };
+
+  return value ? (
+    <textarea {...textareaProps}>{value}</textarea>
+  ) : (
+    <textarea {...textareaProps} />
   );
 };
 
